c91c7a4d526b6c4bafbdb0fb93e035ee6b8878ac
Abdi-Suufi <abdirahman2.suufi@live.uwe.ac.uk>
2023-03-05 18:48:46 +0000

    package was dodgy before

---
+++ src/LuxuryCampSiteGUI/campsiteGUIMain.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package luxurycampsitegui;
+
+/**
+ *
+ * @author abdis
+ */
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class campsiteGUIMain extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(campsiteGUIMain.class.getResource("campsiteGUI.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1237, 825);
+        stage.setTitle("Luxury Camp Site System");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
